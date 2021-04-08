9fc3096aa07d3f667e4d42f5bc5b0a5d4b02de5b
iohana <iohana@ufu.com>
2021-04-07 23:08:07 -0300

    Turorial 3

---
+++ Semana02/prog01.py
@@ -0,0 +1,37 @@
+numA = 3.14
+print(type(numA))
+#-------------------------
+'''
+Adição:  3 + 2 = 5
+Subtração: 3 - 1 = 1
+Multiplicação: 3 * 2 = 6
+Divisão: 3 / 2 = 1.5
+Divisão Inteira: 3 // 2 = 1
+Exponencial: 3 ** 2 = 9
+Resto: 3 % 2 = 1
+'''
+#-------------------------
+numB = 2
+numB *= 10
+print(numB)
+#-------------------------
+print(abs(-3))
+#-------------------------
+print(round(numA, 1))
+#-------------------------
+'''
+É igual a: 3 == 2 False
+Não é igual a: 3 != 2 True
+É maior que: 3 > 2 True
+É menor que: 3 < 2 False
+É maior ou igual a: 3 >= 2 True
+É menor ou igual a: 3 <= 2 False
+'''
+#-------------------------
+numA = '100'
+numB = '200'
+print(numA + numB)
+#-------------------------
+numA = int('100')
+numB = int('200')
+print(numA + numB)
