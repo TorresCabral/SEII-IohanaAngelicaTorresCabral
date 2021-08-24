be91fc2d8d3d3ad5168e247c5b7d5186c42c2ecc
iohana <iohana@ufu.com>
2021-08-24 16:52:52 -0300

    Tutorial 16

---
+++ Semana02/prog12/prog12.py
@@ -0,0 +1,39 @@
+import csv
+'''
+html_output = ''
+names = []
+
+with open('patrons.csv', 'r') as data_file:
+    csv_data = csv.reader(data_file)
+    #print(list(csv_data))
+
+    next(csv_data)
+    next(csv_data)
+
+    for line in csv_data:        
+        print(line)
+        if line[0] == 'No Reward':
+            break
+        names.append(f"{line[0]} {line[1]}")
+
+for name in names:
+    print(name)
+#------------------------
+html_output += f'<p>There are currenly {len(names)} public contributors. Thank You!<\p>'
+html_output += '\n<ul>'
+for name in names:
+    html_output += f'\n\t<li>{name}</li>'
+
+html_output += '\n<\ul>'
+
+print(html_output)
+'''
+#------------------------
+html_output = ''
+names = []
+
+with open('patrons.csv', 'r') as data_file:
+    csv_data = csv.DictReader(data_file)
+    
+    for item in csv_data:
+        print(item)
