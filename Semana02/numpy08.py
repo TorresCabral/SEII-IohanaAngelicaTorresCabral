3f2e895c74a7f69ea9077932f987a463a4232bf6
iohana <iohana@ufu.com>
2021-10-29 20:21:56 -0300

    Linear Algebra: Matrix Operations

---
+++ Semana02/numpy08.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(0, 10, 1000)
+y = np.linspace(0, 10, 1000)
+
+xv, yv = np.meshgrid(x, y)
+zv = xv**2 + yv**2
+plt.contourf(xv, yv, zv, levels = 30)
+plt.colorbar()
