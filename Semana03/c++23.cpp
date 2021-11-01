ba61b8e19f1a4586c28f8f8d3ec07f878590d937
iohana <iohana@ufu.com>
2021-11-01 15:53:02 -0300

    Class methods

---
+++ Semana03/c++23.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Car{
+    public:
+        string name;
+        string color;
+        double price;
+
+        Car(string name, string color, double price){
+            Name: name;
+            Color = color;
+            Price = price;
+        }
+        void getInfo(){
+
+        }
+};
+
+int main()
+{
+    Car myCar("Ford", "red", 50000);
+
+    myCar.getInfo();
+    
+    system("pause>0");
+}
+
+
+
